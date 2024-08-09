7dc2f87c62b49b18a7bdc78b7a413e964549b8c5
Adan Salman <adansalman73@gmail.com>
2024-08-09 17:55:57 +0500

    refactoring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
 import { useRouter } from 'next/router';
+import LoginPage from './login';
 
 const HomePage = () => {
   const router = useRouter();
 
-  const handleClick = (path: string) => {
-    router.push(path);
-  };
-
   return (
     <div>
-      <h1>Home Page</h1>
-      <button onClick={() => handleClick('/login')}>Login</button>
-      <button onClick={() => handleClick('/signup')}>Signup</button>
-      <button onClick={() => handleClick('/profile')}>Profile</button>
+      <LoginPage/>
     </div>
   );
 };
--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -8,21 +8,17 @@ export default function LogoutPage() {
   useEffect(() => {
     const handleLogout = async () => {
       try {
-        // Get the token from local storage or context
         const token = localStorage.getItem('token'); 
 
         if (token) {
           await logoutUser(token);
         }
 
-        // Clear token from local storage or context if necessary
-        localStorage.removeItem('token'); // or clear from context
+        localStorage.removeItem('token');
 
-        // Redirect to login page
         router.push('/auth/login');
       } catch (error) {
         console.error('Logout failed:', error);
-        // Optionally redirect to an error page or show a message
         router.push('/auth/login');
       }
     };
