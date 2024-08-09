f60feda4b4c3d4eb1ba64b3b6860f7eed73cdb1a
Adan Salman <adansalman73@gmail.com>
2024-08-09 17:03:04 +0500

    desgin changes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,46 +1,100 @@
-// pages/login.tsx
-
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+"use client";
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { loginUser } from '@/services/auth';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const data = await loginUser(username, password);
-    if (data.access_token) {
-      localStorage.setItem('token', data.access_token);
-      router.push('/profile');
-    } else {
-      // Handle login failure
-      alert('Login failed');
+    try {
+      const data = await loginUser(username, password);
+      if (data.access_token) {
+        localStorage.setItem('token', data.access_token);
+        router.push('/profile');
+      } else {
+        setError('Login failed');
+      }
+    } catch (err) {
+      console.error('Login error:', err);
+      setError('An error occurred during login');
     }
   };
 
+  const clearError = () => {
+    setError('');
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="flex items-center justify-center min-h-screen bg-black  " style={{fontFamily:"Times New Roman"}}>
+      <div className="container mx-auto p-4 md:p-6 lg:p-12 ">
+        <main className="flex flex-col items-center justify-center w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-12 w-full max-w-5xl">
+            <div className="bg-white rounded-lg shadow-lg p-10 flex flex-col justify-center">
+              <h2 className="text-5xl font-bold mb-6">Welcome Back!</h2>
+
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="block text-lg font-bold mb-2" htmlFor="username">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block text-lg font-bold mb-2" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-3 px-5 text-lg rounded w-full mb-6"
+                >
+                  Login
+                </button>
+                {error && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-4 rounded" role="alert">
+                  <p className='errorMsg'>{error}</p>
+                  <button onClick={clearError} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>}
+              </form>
+
+              <p className="text-gray-600 flex text-lg  items-center justify-center">
+                Don't have an account?&nbsp;
+                <a href="#" onClick={() => router.push('/signup')} className="text-blue-600 hover:text-blue-800">
+                SignUp
+                </a>
+              </p>
+            </div>
+
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img src="/images/login.png" alt="Organic Mind" className="w-full object-cover" />
+              <p className="text-center my-6 text-3xl text-teal-400 font-bold py-4">Humanity NeuroTech</p>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,54 +1,113 @@
-// pages/signup.tsx
-
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+"use client";
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { registerUser } from '@/services/auth';
 
 const SignupPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const data = await registerUser(username, password, email);
-    if (data.id) {
-      // Redirect to login or profile page
-      router.push('/login');
-    } else {
-      // Handle registration failure
-      alert('Registration failed');
+    try {
+      const data = await registerUser(username, password, email);
+      if (data.message) {
+        setError(data.message);
+      } else {
+        router.push('/login');
+      }
+    } catch (err) {
+      setError('An error occurred. Please try again.');
     }
   };
 
+  const clearError = () => {
+    setError('');
+  };
+
   return (
-    <div>
-      <h1>Signup</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Signup</button>
-      </form>
+    <div className="flex items-center justify-center min-h-screen bg-black" style={{fontFamily:"Times New Roman"}}>
+      <div className="container mx-auto p-4 md:p-6 lg:p-12">
+        <main className="flex flex-col items-center justify-center w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-12 w-full max-w-5xl">
+          
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <img src="/images/signup.png" alt="Organic Mind" className="w-full object-cover" />
+              <p className="text-center my-6 text-3xl text-teal-500 font-bold py-4">Humanity NeuroTech</p>
+            </div>
+            <div className="bg-white rounded-lg shadow-lg p-10 flex flex-col justify-center">
+              <h2 className="text-4xl font-bold mb-6">Create Your Account</h2>
+
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="username">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="mb-6 relative">
+                  <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-3 px-5 rounded w-full mb-6 text-lg"
+                >
+                  Signup
+                </button>
+                {error && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-4 rounded" role="alert">
+                  <p className='errorMsg'>{error}</p>
+                  <button onClick={clearError} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>}
+              </form>
+
+              <p className="text-gray-600 flex items-center justify-center text-lg">
+                Already have an account?&nbsp;
+                <a href="#" onClick={() => router.push('/login')} className="text-blue-600 hover:text-blue-800">
+                  Login
+                </a>
+              </p>
+            </div>
+
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
